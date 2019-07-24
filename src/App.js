859ea1b934ae67d54e4b0368b8ad513a1333bdd0
shaurya.puri.2016@live.rhul.ac.uk <Shaurya.Puri.2016@live.rhul.ac.uk>
2019-07-24 23:42:44 +0100

    Landing structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from './assets/Logo.png'
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We are Senti, literally.</h1>
+        <h2>Let's begin by choosing one or more companies to compare</h2>
+    
+        <DropdownButton variant = "info" id="dropdown-variants-info" key="info" title="Add Company">
+            <Dropdown.Item href="#/action-1">Deutsche Bank</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">JP Morgan</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">UBS</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Citi</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Goldman Sachs</Dropdown.Item>
+        </DropdownButton>
+
+        <div className="Buttons-container">
+            <Button variant="primary">Dashboard</Button>
+            <Button variant="secondary">Compare</Button>
+        </div>
       </header>
     </div>
   );

c999f2e00fe1335cc0832dff80e9e9e7ef617009
shaurya.puri.2016@live.rhul.ac.uk <Shaurya.Puri.2016@live.rhul.ac.uk>
2019-08-02 10:16:36 +0100

    Added some basic functionalities

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import logo from './assets/Logo.png'
 import './App.css';
 import Button from 'react-bootstrap/Button'
 import { WithContext as ReactTags } from 'react-tag-input';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import NewsCard from './components/NewsCard'
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
 
 const data = [
-    {name: 'Week 1', DB: 400, JPM: 600, TES: 200},
-    {name: 'Week 2', DB: 300, JPM: 200, TES: 300},
-    {name: 'Week 3', DB: 300, JPM: 300, TES: 350},
-    {name: 'Week 4', DB: 200, JPM: 400, TES: 250},
-    {name: 'Week 5', DB: 278, JPM: 800, TES: 150}
+    {name: 'Jan', DB: 400, JPM: 600, TES: 200},
+    {name: 'Feb', DB: 300, JPM: 200, TES: 300},
+    {name: 'Mar', DB: 300, JPM: 300, TES: 350},
+    {name: 'Apr', DB: 200, JPM: 400, TES: 250},
+    {name: 'May', DB: 278, JPM: 800, TES: 150},
+    {name: 'Jun', DB: 400, JPM: 600, TES: 200},
+    {name: 'Jul', DB: 300, JPM: 200, TES: 300},
+    {name: 'Aug', DB: 300, JPM: 300, TES: 350},
+    {name: 'Sept', DB: 200, JPM: 400, TES: 250},
+    {name: 'Oct', DB: 278, JPM: 800, TES: 150},
+    {name: 'Nov', DB: 200, JPM: 400, TES: 250},
+    {name: 'Dec', DB: 278, JPM: 800, TES: 150}
+];
+
+const articleQuantdata = [
+    {name: 'Week 1', quant: 100},
+    {name: 'Week 2', quant: 150},
+    {name: 'Week 3', quant: 80},
+    {name: 'Week 4', quant: 50},
+    {name: 'Week 5', quant: 300},
+    {name: 'Week 1', quant: 100},
+    {name: 'Week 2', quant: 150},
+    {name: 'Week 3', quant: 80},
+    {name: 'Week 4', quant: 50},
+    {name: 'Week 5', quant: 300},
+    {name: 'Week 4', quant: 50},
+    {name: 'Week 5', quant: 300}
+
 ];
 
 const KeyCodes = {
@@ -35,19 +60,20 @@ class App extends React.Component {
                 { id: 'TES', text: 'Tesla', color:'#e82127'}
              ],
              newsData: [
-                {title:'Title 1', subtitle:'This is a subtitle', text:'Some quick example text to build on the card title and make up the bulk of the cards content', link:'www.google.com'},
+                {title:'Title 1', subtitle:'This is a subtitle', text:'<p className="senti-green">The banks were accused of manipulating futures markets in precious metals through a process known as "spoofing," the US Justice Department and Commodity Futures Trading Commission (CFTC) announced</p>', link:'www.google.com'},
                 {title:'Title 2', subtitle:'This is a subtitle', text:'Some quick example text to build on the card title and make up the bulk of the cards content', link:'www.google.com'},
                 {title:'Title 3', subtitle:'This is a subtitle', text:'Some quick example text to build on the card title and make up the bulk of the cards content', link:'www.google.com'},
                 {title:'Title 4', subtitle:'This is a subtitle', text:'Some quick example text to build on the card title and make up the bulk of the cards content', link:'www.google.com'},
                 {title:'Title 5', subtitle:'This is a subtitle', text:'Some quick example text to build on the card title and make up the bulk of the cards content', link:'www.google.com'}
-            ]
+            ],
+            showModal: false
         };
 
         this.handleDelete = this.handleDelete.bind(this);
         this.handleAddition = this.handleAddition.bind(this);
         this.handleDrag = this.handleDrag.bind(this);
     }
- 
+
     handleDelete(i) {
         const { tags } = this.state;
         this.setState({
@@ -69,21 +95,35 @@ class App extends React.Component {
         // re-render
         this.setState({ tags: newTags });
     }
+    
     render() {
-        const { tags, suggestions, newsData } = this.state;
+        const { tags, suggestions, newsData, showModal } = this.state;
+
+        //Set Company IDs
         const companyIds = [];
         tags.forEach((tag) => {
             companyIds.push([tag.id, tag.color]);
         });
-        console.log(companyIds);
+
+        //Set the Years filter
+        const filterYears = [];
+        const setYearsFilter = (currentYear) => {
+            let i = 0;
+            while(i<8){
+                filterYears.push(currentYear--);
+                i++;
+            }
+        }
+        setYearsFilter(new Date().getFullYear());
+
         return (
             <div className="App">
                 <div className="Navbar">
                     <nav>
-                        <div class="nav-wrapper">
+                        <div className="nav-wrapper">
                         {/* <a href={logo} class="brand-logo">Logo</a> */}
                         <img src={logo} className="App-logo" alt="logo" />
-                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
                             <li><Button className="App-Button" variant="primary">Log Out</Button></li>
                         </ul>
                         </div>
@@ -97,6 +137,18 @@ class App extends React.Component {
                             handleDrag={this.handleDrag}
                             delimiters={delimiters}
                     />
+                    <Form>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Control as="select">
+                            {
+                                filterYears.map((year) => {
+                                    return (<option>{year}</option>)
+                                })
+                            }
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+
                 </div>
                 <div className="results-body">
                     <div className="graph-view">
@@ -104,7 +156,7 @@ class App extends React.Component {
                             <Legend />
                             {
                                 companyIds.map((company) => {
-                                return (<Line type='monotone' dataKey={`${company[0]}`} stroke={`${company[1]}`}/>)
+                                    return (<Line type='monotone' dataKey={`${company[0]}`} stroke={`${company[1]}`}/>)
                                 })
                             }
                             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -117,8 +169,13 @@ class App extends React.Component {
                         <h4>{newsData.length} articles parsed</h4>
                         {
                             newsData.map((news, index) => {
-                                if(index<3){
-                                    return (<NewsCard title={`${news.title}`} subtitle={`${news.subtitle}`} text={`${news.text}`} link={`${news.link}`}/>)
+                                if(index<2){
+                                    return (<NewsCard 
+                                        title={`${news.title}`}
+                                        subtitle={`${news.subtitle}`}
+                                        text={`${news.text}`}
+                                        link={`${news.link}`}
+                                    />)
                                 }
                             })
                         }
--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,14 +1,18 @@
 import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card'
+import renderHTML from 'react-render-html';
+import Button from 'react-bootstrap/Button'
 
 class NewsCard extends React.Component {
+
     render() {
+
         return (
             <Card style={{ width: '22rem' }}>
                 <Card.Body>
                     <Card.Title>{this.props.title}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{this.props.subtitle}</Card.Subtitle>
-                    <Card.Text>{this.props.text}</Card.Text>
+                    <Card.Text>{this.props.text.substring(0, 150)} ...</Card.Text>
                     <Card.Link style={{ color: '#15598A'}} href="#">{this.props.link}</Card.Link>
                 </Card.Body>
             </Card>
